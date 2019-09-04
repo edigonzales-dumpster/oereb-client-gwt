98f1e62f8e9a917af7e85aeedf791a6cb9b26f0c
Stefan Ziegler <edi.gonzales@gmail.com>
2019-09-04 17:40:05 +0200

    start with ol3

--- src/main/java/com/gwidgets/client/AppEntryPoint.java
+++ src/main/java/com/gwidgets/client/AppEntryPoint.java
@@ -1,5 +1,7 @@
 package com.gwidgets.client;
 
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -30,6 +32,36 @@ import gwt.material.design.client.ui.MaterialPreLoader;
 import gwt.material.design.client.ui.MaterialRow;
 import gwt.material.design.client.ui.MaterialSpinner;
 import gwt.material.design.client.ui.MaterialToast;
+import gwt.material.design.client.ui.html.Div;
+import ol.Collection;
+import ol.Coordinate;
+import ol.Map;
+import ol.MapOptions;
+import ol.OLFactory;
+import ol.View;
+import ol.ViewOptions;
+import ol.control.Control;
+import ol.control.Rotate;
+import ol.control.ScaleLine;
+import ol.control.Zoom;
+import ol.interaction.KeyboardPan;
+import ol.interaction.KeyboardZoom;
+import ol.layer.Image;
+import ol.layer.LayerOptions;
+import ol.layer.Tile;
+import ol.proj.Projection;
+import ol.proj.ProjectionOptions;
+import ol.source.ImageWms;
+import ol.source.ImageWmsOptions;
+import ol.source.ImageWmsParams;
+import ol.source.Osm;
+import ol.source.Wmts;
+import ol.source.WmtsOptions;
+import ol.source.XyzOptions;
+import ol.tilegrid.TileGrid;
+import ol.tilegrid.WmtsTileGrid;
+import ol.tilegrid.WmtsTileGridOptions;
+import proj4.Proj4;
 
 public class AppEntryPoint implements EntryPoint {
 
@@ -55,6 +87,11 @@ public class AppEntryPoint implements EntryPoint {
 	    columnRight.setGrid("s8");
 	    columnRight.setBackgroundColor(Color.LIGHT_GREEN);
 	    columnRight.add(new Label("Map belongs here."));
+	    
+	    Div mapDiv = new Div();
+	    mapDiv.setId("mymap");
+	    columnRight.add(mapDiv);
+	    
    
 	    row.add(columnLeft);
         row.add(columnRight);
@@ -64,7 +101,7 @@ public class AppEntryPoint implements EntryPoint {
         card.setTitle("gaga");
         card.setBackgroundColor(Color.BROWN_LIGHTEN_2);
         card.setHeight("200px");
-        card.getElement().getStyle().setProperty("transition", "height 2s");
+        card.getElement().getStyle().setProperty("transition", "height 1s");
         
         
         MaterialCardTitle cardTitle = new MaterialCardTitle();
@@ -178,6 +215,129 @@ public class AppEntryPoint implements EntryPoint {
       ExtractHandler handler = new ExtractHandler();
       button2.addClickHandler(handler);
 
+      // Openlayers
+      
+      
+      // create a projection       
+      Proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
+
+      
+      
+      // TODO 2056 und extent.
+      Projection projection = Projection.get("EPSG:3857");
+
+      // create a OSM-layer
+      XyzOptions osmSourceOptions = OLFactory.createOptions();
+
+      Osm osmSource = new Osm(osmSourceOptions);
+      LayerOptions osmLayerOptions = OLFactory.createOptions();
+      osmLayerOptions.setSource(osmSource);
+
+      Tile osmLayer = new Tile(osmLayerOptions);
+
+      WmtsOptions wmtsOptions = OLFactory.createOptions();
+      wmtsOptions.setUrl("https://services.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_Density/MapServer/WMTS/");
+      wmtsOptions.setLayer("0");
+      wmtsOptions.setFormat("image/png");
+      wmtsOptions.setMatrixSet("EPSG:3857");
+      wmtsOptions.setStyle("default");
+      wmtsOptions.setProjection(projection);
+      wmtsOptions.setWrapX(true);
+      wmtsOptions.setTileGrid(this.createWmtsTileGrid(projection));
+
+      // create attribution
+      wmtsOptions.setAttributions("Tiles &copy; <a href=\"http://services.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_Density/MapServer/\">ArcGIS</a>");
+
+      Wmts wmtsSource = new Wmts(wmtsOptions);
+
+      LayerOptions wmtsLayerOptions = OLFactory.createOptions();
+      wmtsLayerOptions.setSource(wmtsSource);
+
+      Tile wmtsLayer = new Tile(wmtsLayerOptions);
+      wmtsLayer.setOpacity(0.7);
+
+      // create a view
+      ViewOptions viewOptions = OLFactory.createOptions();
+      viewOptions.setProjection(projection);
+      View view = new View(viewOptions);
+
+      Coordinate centerCoordinate = new Coordinate(-11158582, 4813697);
+
+      view.setCenter(centerCoordinate);
+      view.setZoom(4);
+
+      // create the map
+      MapOptions mapOptions = OLFactory.createOptions();
+      mapOptions.setTarget(mapDiv.getId());
+      mapOptions.setView(view);
+
+      Map map = new Map(mapOptions);
+
+      // add layers
+      map.addLayer(osmLayer);
+      map.addLayer(wmtsLayer);
+
+      // add some controls
+      map.addControl(new ScaleLine());
+
+      // add some interactions
+      map.addInteraction(new KeyboardPan());
+      map.addInteraction(new KeyboardZoom());
+      map.addControl(new Rotate());
+
+      
+      
+      
+      
+      
+      
+      
+//      ImageWmsParams imageWMSParams = OLFactory.createOptions();
+//      imageWMSParams.setLayers("ch.swisstopo.geologie-geotechnik-gk500-gesteinsklassierung,ch.bafu.schutzgebiete-paerke_nationaler_bedeutung");
+//
+//      ImageWmsOptions imageWMSOptions = OLFactory.createOptions();
+//      imageWMSOptions.setUrl("http://wms.geo.admin.ch/");
+//      imageWMSOptions.setParams(imageWMSParams);
+//      imageWMSOptions.setRatio(1.5f);
+//
+//      ImageWms imageWMSSource = new ImageWms(imageWMSOptions);
+//
+//      LayerOptions layerOptions = OLFactory.createOptions();
+//      layerOptions.setSource(imageWMSSource);
+//
+//      Image wmsLayer = new Image(layerOptions);
+//
+//      // create a projection
+//      ProjectionOptions projectionOptions = OLFactory.createOptions();
+//      projectionOptions.setCode("EPSG:21781");
+//      projectionOptions.setUnits("m");
+//
+//      Projection projection = new Projection(projectionOptions);
+//      // projection.setExtent()...
+//
+//      // create a view
+//      ViewOptions viewOptions = OLFactory.createOptions();
+//      viewOptions.setProjection(projection);
+//      View view = new View(viewOptions);
+//     
+//      Coordinate centerCoordinate = new Coordinate(660000, 190000);
+//
+//      view.setCenter(centerCoordinate);
+//      view.setZoom(9);
+//
+//      // create the map
+//      MapOptions mapOptions = OLFactory.createOptions();
+//      mapOptions.setTarget(mapDiv.getId());
+//      mapOptions.setView(view);
+//      mapOptions.setControls(new Collection<Control>());
+//
+//      Map map = new Map(mapOptions);
+//
+//      map.addLayer(wmsLayer);
+
+
+
+     
 
      
 /*
@@ -357,4 +517,45 @@ public class AppEntryPoint implements EntryPoint {
 		
 	}
 
+    private TileGrid createWmtsTileGrid(Projection projection) {
+
+        WmtsTileGridOptions wmtsTileGridOptions = OLFactory.createOptions();
+
+        double[] resolutions = new double[14];
+        String[] matrixIds = new String[14];
+
+        double width = projection.getExtent().getWidth();
+        double matrixWidth = width / 256;
+
+        for (int i = 0; i < 14; i++) {
+            resolutions[i] = matrixWidth / Math.pow(2, i);
+            matrixIds[i] = String.valueOf(i);
+        }
+
+        Coordinate tileGridOrigin = projection.getExtent().getTopLeft();
+        wmtsTileGridOptions.setOrigin(tileGridOrigin);
+        wmtsTileGridOptions.setResolutions(resolutions);
+        wmtsTileGridOptions.setMatrixIds(matrixIds);
+
+        return new WmtsTileGrid(wmtsTileGridOptions);
+
+    }
+    
+    private TileGrid getWmtsTileGrid(Projection projection) {
+        WmtsTileGridOptions wmtsTileGridOptions = OLFactory.createOptions();
+
+        double resolutions[] = new double[] {4000.0,2000.0,1000.0,500.0,250.0,100.0,50.0,20.0,10.0,5.0,2.5,1.0,0.5,0.25,0.1};
+        String[] matrixIds = new String[resolutions.length];
+        
+        for (int z = 0; z < resolutions.length; ++z) {
+            matrixIds[z] = String.valueOf(z);
+          }
+        
+        Coordinate tileGridOrigin = projection.getExtent().getTopLeft();
+        wmtsTileGridOptions.setOrigin(tileGridOrigin);
+        wmtsTileGridOptions.setResolutions(resolutions);
+        wmtsTileGridOptions.setMatrixIds(matrixIds);
+
+        return new WmtsTileGrid(wmtsTileGridOptions);
+    }
 }
